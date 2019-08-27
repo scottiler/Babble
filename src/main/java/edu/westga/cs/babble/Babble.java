cb2d132a6284a65e854c9a4caa6d0b8783a22048
Perry Iler <piler1@my.westga.edu>
2019-08-27 14:18:23 -0400

    Setup BabbleGUI layout in scene builder

--- src/main/java/edu/westga/cs/babble/Babble.java
+++ src/main/java/edu/westga/cs/babble/Babble.java
@@ -19,14 +19,14 @@ public class Babble extends Application {
 	private static final String GUI_RESOURCE = "edu/westga/cs/babble/views/BabbleGui.fxml";
     @Override
     public void start(Stage primaryStage) throws Exception {
-//        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-//        URL resource = classLoader.getResource(Babble.GUI_RESOURCE);
-//        FXMLLoader loader = new FXMLLoader(resource);
-//        Parent root = (Parent) loader.load();
-//        Scene scene = new Scene(root);
-//        primaryStage.setScene(scene);
-//        primaryStage.setTitle("Babble");
-//        primaryStage.show();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        URL resource = classLoader.getResource(Babble.GUI_RESOURCE);
+        FXMLLoader loader = new FXMLLoader(resource);
+        Parent root = (Parent) loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Babble");
+        primaryStage.show();
     }
 
     /**
