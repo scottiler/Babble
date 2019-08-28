2c6be5865dccc4cd0bf851fd2644c4681978e418
Perry Iler <piler1@my.westga.edu>
2019-08-28 15:10:32 -0400

    Encountered error at runtime

---
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -0,0 +1,35 @@
+package edu.westga.cs.babble.controllers;
+
+import edu.westga.cs.babble.model.Tile;
+import edu.westga.cs.babble.model.TileBag;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class BabbleController implements Initializable {
+	private TileBag bag;
+	@FXML private ListView tiles;
+	@FXML private Button test;
+	@FXML private TextField text;
+	
+	public void setText() {
+		this.text.setText("it worked");
+	}
+	
+	public void initTiles() {
+		this.tiles.getItems().
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		this.tiles = new ListView<Tile>();
+		this.test = new Button();
+		this.text = new TextField();
+		
+	}
+
+}
