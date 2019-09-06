8b2d17fd4abd56aa18dcd0d6760e15ab7bf7855c
Perry Iler <piler1@my.westga.edu>
2019-09-05 22:16:39 -0400

    Created TestPlayedWordGetScore test class

--- src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
+++ src/test/java/edu/westga/cs/babble/model/TestPlayedWordGetScore.java
@@ -9,22 +9,32 @@ class TestPlayedWordGetScore {
 	
 	@Test
 	void emptyWordShouldHaveScoreOfZero() {
-		this.play.
+		assertEquals(0, this.play.getScore());
 	}
 	
 	@Test
 	void scoreAOneTileWord() {
-		
+		this.play.append(new Tile('a'));
+		assertEquals(1, this.play.getScore());
 	}
 	
 	@Test
 	void scoreAWordWithMultipleDifferingTiles() {
-		
+		this.play.append(new Tile('a'));
+		this.play.append(new Tile('r'));
+		this.play.append(new Tile('m'));
+		this.play.append(new Tile('y'));
+		assertEquals(9, this.play.getScore());
 	}
 	
 	@Test
 	void scoreAWordContainingDuplicateTiles() {
-		
+		this.play.append(new Tile('h'));
+		this.play.append(new Tile('a'));
+		this.play.append(new Tile('p'));
+		this.play.append(new Tile('p'));
+		this.play.append(new Tile('y'));
+		assertEquals(15, this.play.getScore());
 	}
 
 }
